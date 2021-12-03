a9668961c2e74249f60ad022c463cb3e9a7b2aea
AlTsao <al.tsao.yw@gmail.com>
2021-12-03 10:38:23 +0800

    revise

--- COVID19.py
+++ COVID19.py
@@ -27,14 +27,27 @@ act = '#fe9801'
 
 #載入資料
 df = pd.read_csv('covid_19_data_cleaned.csv', parse_dates = ['Date'])
+df['Province/State'] = df['Province/State'].fillna("") #把欄位中的NaN用空格取代
 country_daywise = pd.read_csv('country_daywise.csv', parse_dates = ['Date'])
 countrywise = pd.read_csv('countrywise.csv')
 daywise = pd.read_csv('daywise.csv', parse_dates = ['Date'])
 
+#整理資料
 confirmed = df.groupby('Date').sum()['Confirmed'].reset_index()
 recovered = df.groupby('Date').sum()['Recovered'].reset_index()
 deaths = df.groupby('Date').sum()['Deaths'].reset_index()
 
+#檢視資料
+# print(df.isnull().sum()) #查看是否有空值
+# print(df.info())
+
+#篩選資料
+df.query('Country == "US"').groupby('Date').sum()['Confirmed'].reset_index()
+mask = df['Country'] == 'US'
+df[mask].groupby('Date').sum()['Confirmed'].reset_index()
+
+
+#繪圖
 fig = go.Figure()
 fig.add_trace(go.Scatter(x = confirmed['Date'], y = confirmed['Confirmed']))
 fig.show()
